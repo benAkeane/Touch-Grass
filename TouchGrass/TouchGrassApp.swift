861bd4a236f7b489cee0802b3934486a8e12415d
benAkeane <benkeane@ip0af55a19.int.uvm.edu>
2025-10-08 14:55:42 -0400

    created xcode project added .gitignore file

---
+++ TouchGrass/TouchGrassApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchGrassApp.swift
+//  TouchGrass
+//
+//  Created by Ben Keane on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TouchGrassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TouchGrassTests/TouchGrassTests.swift
@@ -0,0 +1,17 @@
+//
+//  TouchGrassTests.swift
+//  TouchGrassTests
+//
+//  Created by Ben Keane on 10/8/25.
+//
+
+import Testing
+@testable import TouchGrass
+
+struct TouchGrassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
