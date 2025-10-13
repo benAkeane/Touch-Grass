627031da270bd80505b7e06097c42393ee2f0963
marcodrubens <mrubens@uvm.edu>
2025-10-13 15:32:51 -0400

    added example map with button

--- TouchGrass/ContentView.swift
+++ TouchGrass/ContentView.swift
@@ -6,16 +6,47 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    // example pins
+    let mercatCentral = CLLocationCoordinate2D(latitude: 41.115908,
+                                               longitude: 1.248912)
+    let balco = CLLocationCoordinate2D(latitude: 41.113694,
+                                       longitude: 1.256485)
+    let abuelos = CLLocationCoordinate2D(latitude: 41.113518,
+                                         longitude: 1.253227)
+    let burlington = CLLocationCoordinate2D(latitude: 44.477095,
+                                            longitude: -73.212567)
+    
+    @State var camera: MapCameraPosition = .automatic
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map(position: $camera) {
+            Marker("Mercat Central", coordinate: mercatCentral)
+                .tint(.green)
+            Marker("Balcó del Mediterrani", coordinate: balco)
+                .tint(.blue)
+            Marker("Abuelos", coordinate: abuelos)
+                .tint(.purple)
         }
-        .padding()
+        // container for buttons
+        .safeAreaInset(edge: .bottom) {
+            HStack {
+                Spacer()
+                Button {
+                    camera = .region(MKCoordinateRegion(center: burlington, latitudinalMeters: 2000, longitudinalMeters: 2000))
+                    
+                } label: {
+                    Text("Go to Burlington")
+                }
+                Spacer()
+            }
+            .padding(.top)
+            .background(.thinMaterial)
+        }
+        .mapStyle(.standard)
     }
 }
 
