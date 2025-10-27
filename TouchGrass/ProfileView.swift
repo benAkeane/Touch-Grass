b553a47f9b3a816c7da873d199b4f1bdf1976e69
Tim M <153383959+TimMsr@users.noreply.github.com>
2025-10-27 13:15:49 -0400

    Added ProfileView

---
+++ TouchGrass/ProfileView.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileView.swift
+//  TouchGrass
+//
+//  Created by Tim Mäser on 10.27.25
+//
+
+import SwiftUI
+import FirebaseAuth
+
+struct ProfileView: View {
+    @EnvironmentObject var firebaseFunctions: FirebaseFunctions
+    
+    
+    var body: some View {
+        VStack {
+            Text("Touch Grass")
+                .font(.largeTitle)
+                .bold()
+                .padding(.top)
+            
+            Spacer()
+            
+            }
+            .padding()
+            
+            Spacer()
+    }
+      
+}
+    
+
